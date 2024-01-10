52c86263e3f4cb14e92ec498f98f6e92a7c8aee7
Sanjay Poudel <155279811+xanjayp@users.noreply.github.com>
2024-01-10 09:41:13 +0545

    edited

--- js/function/index.js
+++ js/function/index.js
@@ -1,6 +1,6 @@
 // Write a JavaScript function that reverses a number. Example x = 32243; Expected Output : 34223
 const rev = (word) => {
-  const result = word.split("").reduce((acc, char) => char + acc, "");
+  const result = word.split("").reduce((a, cb) => a + b, "");
   console.log(result);
 };
 rev("hey");
